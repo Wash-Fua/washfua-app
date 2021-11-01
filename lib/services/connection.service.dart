5ad301c0fc7e4dd8576fce22e83ef64d529dbebb
Daniel Dennis <dankatdennis@gmail.com>
2021-11-01 11:53:16 +0300

    - setup connectivity services

---
+++ lib/services/connection.service.dart
@@ -0,0 +1,21 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class ConnectionService extends GetxService {
+  late final Logger logger = Logger();
+
+  Future<ConnectionService> init() async {
+    Connectivity connectivity = Connectivity();
+
+    connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.none) {
+        snackBar(
+          'Connection',
+          'You have been disconnected! The application requires an internet connection to work.',
+          Snack.INFO,
+        );
+      }
+    });
+    return this;
+  }
+}
