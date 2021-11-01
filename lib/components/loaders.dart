ea51447d3a23ddd1d686d781de588ac3ee5b3745
Daniel Dennis <dankatdennis@gmail.com>
2021-11-01 12:03:16 +0300

    - did the same with loaders

---
+++ lib/components/loaders.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:washwash/themes/colors.dart';
+
+Widget smallGreenPulse = SpinKitPulse(color: primary, size: 16);
+Widget smallGreenThree = SpinKitThreeBounce(color: primary, size: 16);
+
+Widget mediumGreenPulse = SpinKitPulse(color: primary, size: 18);
+Widget mediumGreenThree = SpinKitThreeBounce(color: primary, size: 18);
+
+Widget largeWhitePulse = SpinKitPulse(color: white, size: 20);
+Widget largeGreenPulse = SpinKitPulse(color: primary, size: 20);
+
+Widget largeWhiteThree = SpinKitThreeBounce(color: white, size: 20);
+Widget largeGreenThree = SpinKitThreeBounce(color: primary, size: 20);
+
+Widget massiveGreenPulse = SpinKitPulse(color: primary, size: 40);
+Widget massiveGreenThree = SpinKitThreeBounce(color: primary, size: 40);
